fa8884194e190e445840e5d553ad9c8753172de0
Filip Alac <filipalac@gmail.com>
2023-05-17 16:38:17 +0200

    Write Task vector to file in JSON

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,20 +1,15 @@
-/*
-Your task is to implement a to-do list application using Rust. The application should allow users to create new tasks, mark tasks as completed, and view a list of all tasks.
-
-Here are the specific requirements for the application:
-
-Users should be able to add new tasks to the to-do list.
-Users should be able to mark tasks as completed.
-Users should be able to view a list of all tasks, with completed tasks displayed separately from incomplete tasks.
-Tasks should be persisted across application launches.
-The application should have a CLI interface, with subcommands for adding tasks, marking tasks as completed, and viewing the task list.
-
-*/
-
-
 use std::env;
 use std::process;
+use serde::{Serialize, Deserialize};
+use std::fs::File;
+use std::io::Write;
+use std::io::Read;
+//use std::io::BufReader;
+//use std::error::Error;
 
+
+
+#[derive(Serialize, Deserialize, Debug)]
 struct Task {
     task: String,
     completed: bool,
@@ -48,24 +43,32 @@ fn mark_complete(tasks: &mut Vec<Task>, position: usize) {
     }
 }
 
-fn print_list(tasks: &mut Vec<Task>) {
+fn print_list(tasks: &Vec<Task>) {
     for task in tasks {
         println!("[{}] {}",if task.completed {"x"} else {""},  task.task);
     }
 }
+
+fn write_list_to_file(tasks: &Vec<Task>) {
+        let serialized = serde_json::to_string(&tasks).unwrap();
+        let mut file = File::create("tasks.json").unwrap();
+        file.write_all(serialized.as_bytes()).unwrap();
+}
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let mut tasks: Vec<Task> = Vec::new();
 
-   //push_task(&mut tasks, "ASDASDASDASD");
-   //push_task(&mut tasks, "ASDASDASDASD");
-    //mark_complete(&mut tasks, 1);
-   //print_list(&mut tasks);
+   push_task(&mut tasks, "ASDASDASDASD");
+   push_task(&mut tasks, "ASDASDASDASD");
+   mark_complete(&mut tasks, 1);
+   print_list(&tasks);
+   write_list_to_file(&tasks);
+
 
     if args.len() == 2 && &args[1] == "list" {
-        println!("Listy DPC");
     } else if args.len() == 3 && &args[1] == "complete" {
 
         let complete = parse_complete_num(&args[2]);
@@ -76,12 +79,10 @@ fn main() {
 
     } else if  args.len() == 3 && &args[1] == "add" {
         let add = &args[2];
-        println!("add {}", add);
         push_task(&mut tasks, &args[2]);
 
 
     } else {
-        println!("Bad input, exiting");
         process::exit(0);
     }
 
