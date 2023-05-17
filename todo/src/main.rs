8f3091ea42356478f57da6a1d2b1bf58e744758b
Filip Alac <filipalac@gmail.com>
2023-05-17 13:46:07 +0200

    Added uncomplete parsing of argv

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,3 +1,37 @@
+/*
+Your task is to implement a to-do list application using Rust. The application should allow users to create new tasks, mark tasks as completed, and view a list of all tasks.
+
+Here are the specific requirements for the application:
+
+Users should be able to add new tasks to the to-do list.
+Users should be able to mark tasks as completed.
+Users should be able to view a list of all tasks, with completed tasks displayed separately from incomplete tasks.
+Tasks should be persisted across application launches.
+The application should have a CLI interface, with subcommands for adding tasks, marking tasks as completed, and viewing the task list.
+
+*/
+
+
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+
+    if args.len() == 2 && &args[1] == "list" {
+        println!("Listy DPC");
+    } else if args.len() == 3 && &args[1] == "complete" {
+        println!("complete {}", &args[2]);
+    } else if  args.len() == 3 && &args[1] == "add" {
+        println!("add {}", &args[2]);
+    } else {
+        println!("Bad input, exiting");
+        process::exit(0);
+    }
+
+
+
+
+
 }
